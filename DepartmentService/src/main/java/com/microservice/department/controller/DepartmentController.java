34888f588736409b32e7c70707b1e125ebd0d468
my_admin-at-534591548547 <mahmalli@gmail.com>
2022-01-01 15:43:31 -0500

    First code commit for Dept SpringBoot API

---
+++ DepartmentService/src/main/java/com/microservice/department/controller/DepartmentController.java
@@ -0,0 +1,35 @@
+package com.microservice.department.controller;
+
+import com.microservice.department.entity.Department;
+import com.microservice.department.service.DepartmentService;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+    final static Logger log = LoggerFactory.getLogger(DepartmentController.class);
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of DepartmentController");
+        return  departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+        log.info("Inside findDepartmentById method of DepartmentController");
+        return departmentService.findDepartmentById(departmentId);
+    }
+
+}
---
+++ DepartmentService/src/main/java/com/microservice/department/service/DepartmentService.java
@@ -0,0 +1,30 @@
+package com.microservice.department.service;
+
+import com.microservice.department.entity.Department;
+import com.microservice.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    final static Logger log = LoggerFactory.getLogger(DepartmentService.class);
+    
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
