167ca067e8d0e33555a5a21646817a08c5b9c61b
my_admin-at-534591548547 <mahmalli@gmail.com>
2022-01-01 18:33:06 -0500

    First commit
    Signed-off-by: my_admin-at-534591548547 <mahmalli@gmail.com>

---
+++ spring-hello-git/src/main/java/com/test/hello/SpringHelloGitApplication.java
@@ -0,0 +1,13 @@
+package com.test.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloGitApplication.class, args);
+	}
+
+}
